6d56c3654485a67071d3a19e5591ebce3f497139
anglal <lalshwarlimbu@gmail.com>
2023-05-02 01:03:58 -0700

    Backend updated

--- note-app-backend/src/main/java/noteappbackend/controller/NoteController.java
+++ note-app-backend/src/main/java/noteappbackend/controller/NoteController.java
@@ -1,8 +1,14 @@
 package noteappbackend.controller;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,14 +18,39 @@ import noteappbackend.service.NoteService;
 @RestController
 @RequestMapping("/")
 public class NoteController {
-private NoteService noteService;
+	private NoteService noteService;
 
-public NoteController(NoteService noteService) {
-	this.noteService = noteService;
-}
+	public NoteController(NoteService noteService) {
+		this.noteService = noteService;
+	}
 
-@GetMapping("/notes")	
-public List<Note> getNotes() {
-	return this.noteService.getNotes();
-}
+	@GetMapping("/notes")
+	public List<Note> getNotes() {
+		return this.noteService.getNotes();
+	}
+
+	@GetMapping("/notes/{id}")
+	private Note getNote(@PathVariable("id") int noteId) {
+		try {
+			return this.noteService.getNote(noteId);
+		} catch (NoSuchElementException e) {
+			return null;
+		}
+
+	}
+	
+	@PostMapping("/notes")
+	public Note createNote(@RequestBody Note note) {
+		return this.noteService.createNote(note);
+	}
+	
+	@PatchMapping("/notes/{id}")
+	public Note editNote(@PathVariable("id") Integer noteId, @RequestBody Note note) {
+		return this.noteService.edit(noteId, note);
+	}
+	
+	@DeleteMapping("/notes/{id}")
+	public void deleteNote(@PathVariable("id") Integer noteId) {
+		this.noteService.deleteNote(noteId);
+	}
 }
--- note-app-backend/src/main/java/noteappbackend/service/NoteService.java
+++ note-app-backend/src/main/java/noteappbackend/service/NoteService.java
@@ -2,6 +2,8 @@ package noteappbackend.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
@@ -30,6 +32,46 @@ public class NoteService {
 			noteDtos.add(noteDto);
 		}
 		return noteDtos;
+	}
+
+	public Note getNote(int noteId) {
+		NoteEntity noteEntity = this.noteRepository.findById(noteId).get();
+		Note noteDto = new Note();
+		BeanUtils.copyProperties(noteEntity, noteDto);
+		return noteDto;
+	}
+
+	public Note createNote(Note note) {
+		NoteEntity noteEntity = new NoteEntity();
+		BeanUtils.copyProperties(note, noteEntity);
+		NoteEntity noteEntityReturned = this.noteRepository.save(noteEntity);
+		Note noteReturned = new Note();
+		BeanUtils.copyProperties(noteEntityReturned, noteReturned);
+		return noteReturned;
+	}
+
+	public Note edit(Integer noteId, Note note) {
+		try {
+			NoteEntity noteFound = this.noteRepository.findById(noteId).get();
+
+			if(null != note.getTitle()) {
+				noteFound.setTitle(note.getTitle());
+			}
+			if(null != note.getBody()) {
+				noteFound.setBody(note.getBody());
+			}			
+			NoteEntity returnedNoteEntity = this.noteRepository.save(noteFound);
+			Note returnedNote = new Note();
+			BeanUtils.copyProperties(returnedNoteEntity, returnedNote);
+			return returnedNote;
+		} catch (NoSuchElementException e) {
+			return null;
+		}
+
+	}
+
+	public void deleteNote(Integer noteId) {
+		this.noteRepository.deleteById(noteId);
 	}
 
 }
